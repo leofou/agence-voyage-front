4e49294389547cdaac82eccab98599bf1c481f6f
joachim napias <joachim.napias@gmail.com>
2021-01-27 18:07:56 +0100

    début customercomponent

--- paper-kit-2-angular-master/src/app/components/components.module.ts
+++ paper-kit-2-angular-master/src/app/components/components.module.ts
@@ -14,6 +14,7 @@ import { ComponentsComponent } from './components.component';
 import { NotificationComponent } from './notification/notification.component';
 import { NgbdModalComponent } from './modal/modal.component';
 import { NgbdModalContent } from './modal/modal.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
     imports: [
@@ -32,7 +33,8 @@ import { NgbdModalContent } from './modal/modal.component';
         NucleoiconsComponent,
         NotificationComponent,
         NgbdModalComponent,
-        NgbdModalContent
+        NgbdModalContent,
+        CustomerComponent
     ],
     entryComponents: [NgbdModalContent],
     exports:[ ComponentsComponent ]
---
+++ paper-kit-2-angular-master/src/app/components/customer/customer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from 'app/model/customer';
+import { CustomerService } from 'app/service/customer.service';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customers: Customer[];
+  customer: Customer= new Customer();
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+  findAll() {
+    this.customerService.findAll().subscribe(data => { this.customers = data });
+  }
+
+
+}
